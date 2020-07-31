9720aadd21bafb20ad2a836cca1339087be4a0b4
Eugenio Cunha <eugenio.cunha@ymail.com>
2020-07-31 16:45:36 -0300

    implementado o envio de formularios em paralelo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # apothecary
-BOT apothecary
+
+Automatiza o envio de mensagens, informando ao Boticário sobre cobranças indevidas
+
+> Requer o google chrome instalado para manipulação do browser
+```sh
+# run 
+go run main.go
+```
+
--- main.go
+++ main.go
@@ -1,64 +1,125 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/chromedp/cdproto/target"
 	"github.com/chromedp/chromedp"
 )
 
+// Tab contém o contexto de uma aba no google chrome
+type Tab struct {
+	context context.Context
+	cancel  context.CancelFunc
+}
+
 func main() {
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-    chromedp.Flag("headless", true),
-    chromedp.Flag("disable-gpu", false),
-    chromedp.Flag("enable-automation", false),
-    chromedp.Flag("disable-extensions", false),
+	options := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("enable-automation", true),
+		chromedp.Flag("restore-on-startup", false),
+		chromedp.Flag("disable-notification", true),
+		chromedp.Flag("disable-checker-imaging", true),
+		chromedp.Flag("no-default-browser-check", true),
+		chromedp.Flag("ignore-certificate-errors", true),
+		chromedp.Flag("disable-accelerated-2d-canvas", true),
+		chromedp.Flag("disable-background-networking", true),
+		chromedp.Flag("enable-features", "NetworkService,NetworkServiceInProcess"),
+		chromedp.Flag("disable-background-timer-throttling", true),
+		chromedp.Flag("disable-backgrounding-occluded-windows", true),
+		chromedp.Flag("disable-breakpad", true),
+		chromedp.Flag("disable-client-side-phishing-detection", true),
+		chromedp.Flag("disable-default-apps", true),
+		chromedp.Flag("disable-dev-shm-usage", true),
+		chromedp.Flag("disable-extensions", false),
+		chromedp.Flag("disable-features", "site-per-process,TranslateUI,BlinkGenPropertyTrees"),
+		chromedp.Flag("disable-hang-monitor", true),
+		chromedp.Flag("disable-ipc-flooding-protection", true),
+		chromedp.Flag("disable-popup-blocking", true),
+		chromedp.Flag("disable-prompt-on-repost", true),
+		chromedp.Flag("disable-renderer-backgrounding", true),
+		chromedp.Flag("disable-sync", true),
+		chromedp.Flag("force-color-profile", "srgb"),
+		chromedp.Flag("metrics-recording-only", true),
+		chromedp.Flag("safebrowsing-disable-auto-update", true),
+		chromedp.Flag("enable-automation", true),
+		chromedp.Flag("password-store", "basic"),
+		chromedp.Flag("use-mock-keychain", true),
+		chromedp.Flag("blink-settings", "imagesEnabled=false"),
 	)
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	allocCtx, _ := chromedp.NewExecAllocator(context.Background(), options...)
 	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
-	count := 0
-	for {
-		err := chromedp.Run(ctx, sendkeys(
-			"Não sou a Rafaela, parem de enviar cobranças", 
-			"Não sou a Rafaela, parem de enviar cobranças", 
-			"00000000000",
-			"00000000000",
-			"00000000000",
-			"nao.sou.a.rafaela@nao.sou.a.rafaela.com",
-			"Não sou a Rafaela, parem de enviar cobranças",
-			"Pagamento",
-			"Cobrança Indevida/Cupom Fiscal",
-			"Não sou a Rafaela, parem de enviar cobranças para (31) 9 99742-7487",
-			"Não"))
-		if err != nil {
-			log.Fatal(err)
-		}
+	err := chromedp.Run(ctx)
+	checkErr(err)
 
-		count++
-		log.Printf("Count: `%d`", count)
+	sent := 0
+	for {
+		targets, err := chromedp.Targets(ctx)
+		checkErr(err)
+		if countTabs(targets) < 10 {
+
+			tab := newTab(ctx)
+			go fillForm(tab)
+			sent++
+			log.Print(sent)
+		}
 	}
 }
 
-func sendkeys(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11 string) chromedp.Tasks { 
-	return chromedp.Tasks{
+// checkErr testa se existe um error
+func checkErr(err error) {
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+// newTab abre uma nova aba no google chrome
+func newTab(ctx context.Context) Tab {
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	ctx, _ = chromedp.NewContext(ctx)
+	return Tab{context: ctx, cancel: cancel}
+}
+
+// fillForm preenche e envia um formulário
+func fillForm(tab Tab) {
+	err := chromedp.Run(tab.context, chromedp.Tasks{
 		chromedp.Navigate(`https://grupoboticario.secure.force.com/formularioWeb?customId=CRCBOT`),
 		chromedp.WaitVisible(`//*[@id="j_id0:frm"]`),
-		chromedp.SendKeys(`#a2Q1U000000SPHjUAO`, v1, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHgUAO`, v2, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHeUAO`, v3, chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHjUAO`, "Não sou a Rafaela, parem de enviar cobranças", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHgUAO`, "Não sou a Rafaela, parem de enviar cobranças", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHeUAO`, "00000000000", chromedp.ByID),
 		chromedp.SetAttributeValue(`#a2Q1U000000SPHeUAO`, "data-role", ""),
-		chromedp.SendKeys(`#a2Q1U000000SPHkUAO`, v4, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHiUAO`, v5, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHnUAO`, v6, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHlUAO`, v7, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHmUAO`, v8, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHfUAO`, v9, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000000SPHhUAO`, v10, chromedp.ByID),
-		chromedp.SendKeys(`#a2Q1U000001dnlJUAQ`, v11, chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHkUAO`, "00000000000", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHiUAO`, "00000000000", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHnUAO`, "nao.sou.a.rafaela@nao.sou.a.rafaela.com", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHlUAO`, "Não sou a Rafaela, parem de enviar cobranças", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHmUAO`, "Pagamento", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHfUAO`, "Cobrança Indevida/Cupom Fiscal", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000000SPHhUAO`, "Não sou a Rafaela, parem de enviar cobranças para (00) 0 00000-0000", chromedp.ByID),
+		chromedp.SendKeys(`#a2Q1U000001dnlJUAQ`, "Não", chromedp.ByID),
 		chromedp.Click(`//*[@id="j_id0:frm:j_id11"]/div[2]/div/div`, chromedp.BySearch),
 		chromedp.WaitVisible(`body > div > p > i > b`),
+	})
+	checkErr(err)
+	tab.cancel()
+}
+
+// countTabs conta quantas abas foram abertas no google chrome
+func countTabs(targets []*target.Info) int {
+	result := 0
+	for _, t := range targets {
+		if t.Type == "page" {
+			result++
+		}
 	}
+	return result
 }
